45a54b44a563dacc35d18937cf3db6495edc1b1d
Max <acmxca@gmail.com>
2016-11-13 12:32:18 +0000

    Correct return values

--- tokenanalyzer.cpp
+++ tokenanalyzer.cpp
@@ -7,7 +7,7 @@ TokenType TokenAnalyzer::get_token_type(const std::string &token)
         size_t indx = 0;
         std::stold(token, &indx);
         if (indx == token.size()) {
-            return number;
+            return TAnumber;
         }
     } catch (...) {
         //continue
@@ -15,20 +15,20 @@ TokenType TokenAnalyzer::get_token_type(const std::string &token)
     //try compare with func names -> return func
     for (int i = 0; i < (int)m_funcs.size(); i++) {
         if (token == m_funcs[i]) {
-            return func;
+            return TAfunc;
         }
     }
     //check for operation -> return operation
     if (token.size() == 1) {
         for (int i = 0; m_operations[i] != 0; i++) {
             if (token[0] == m_operations[i]) {
-                return operation;
+                return TAoperation;
             }
         }
     }
     // check for var name
     if (!std::isdigit(token[0])) {
-        return var;
+        return TAvar;
     }
-    return trash;
+    return TAtrash;
 }
